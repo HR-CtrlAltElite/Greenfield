588f43ca9fa7586f6d572a19343b144803f92e8b
Benjamin Nicholson <robertsonorama@gmail.com>
2025-08-07 10:54:45 -0700

    adding css module configuration and style samples

--- client/src/reviews/components/ReviewTile.jsx
+++ client/src/reviews/components/ReviewTile.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import * as styles from '../reviews.module.css';
 /* eslint-disable react/prop-types */
 
 function ReviewsList({ review }) {
   return (
-    <div className="review-tile-wrapper">
-      <h2>Review Tile:</h2>
-      <p>{review.review_id}</p>
+    <div className={styles.reviewTileWrapper}>
+      <div className={styles.reviewTileHeading}>
+        <div className={styles.reviewTileStarRating}>
+          ☆☆☆☆☆:
+          {review.rating}
+        </div>
+        <div className="review-title__authorship">
+          <span className="review-title__review-user">
+            {review.reviewer_name}
+          </span>
+          <span className="review-title__review-date">
+            {review.date}
+          </span>
+        </div>
+      </div>
+      <h2 className="review-tile__summary">{review.summary}</h2>
+      <p className="review-tile__body">{review.body}</p>
+      <p className="review-tile__recommended">✓ I recommend this product</p>
+      <div className={styles.reviewTitleResponse}>
+        <h3 className={styles.reviewTitleResponseTitle}>Response: </h3>
+        <p className="review-title__response-title">{review.response}</p>
+      </div>
+      <div className="review-tile__helpfulness-footer">
+        <p className="review-tile__helpfulness-text">
+          <span className="review-tile__helpful-btn">
+            Helpful?
+            <button type="button">Yes</button>
+            {review.helpfulness}
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
